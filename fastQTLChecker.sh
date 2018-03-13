efe90d68975cb2573520e438c3def4050e968e8f
swvanderlaan <s.w.vanderlaan@gmail.com>
2018-03-13 13:03:32 +0100

    Bug fixing shizzle

--- fastQTLChecker.sh
+++ fastQTLChecker.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
-#
-# You can use the variables below (indicated by "#$") to set some things for the 
-# submission system.
-# -S /bin/bash # the type of BASH you'd like to use
-# -N fastQTLChecker_v1 # the name of this script
-# -hold_jid some_other_basic_bash_script # the current script (basic_bash_script) will hold until some_other_basic_bash_script has finished
-# -o /hpc/dhl_ec/svanderlaan/projects/test_mqtl/fastQTLChecker_v1.log # the log file of this job
-# -e /hpc/dhl_ec/svanderlaan/projects/test_mqtl/fastQTLChecker_v1.errors # the error file of this job
-# -l h_rt=04:00:00 # h_rt=[max time, hh:mm:ss, e.g. 02:02:01] - this is the time you think the script will take
-# -l h_vmem=8G #  h_vmem=[max. mem, e.g. 45G] - this is the amount of memory you think your script will use
-# -l tmpspace=32G # this is the amount of temporary space you think your script will use
-# -M s.w.vanderlaan-2@umcutrecht.nl # you can send yourself emails when the job is done; "-M" and "-m" go hand in hand
-# -m ea # you can choose: b=begin of job; e=end of job; a=abort of job; s=suspended job; n=no mail is send
-# -cwd # set the job start to the current directory - so all the things in this script are relative to the current directory!!!
-#
-# Another useful tip: you can set a job to run after another has finished. Name the job 
-# with "-N SOMENAME" and hold the other job with -hold_jid SOMENAME". 
-# Further instructions: https://wiki.bioinformatics.umcutrecht.nl/bin/view/HPC/HowToS#Run_a_job_after_your_other_jobs
-#
-# It is good practice to properly name and annotate your script for future reference for
-# yourself and others. Trust me, you'll forget why and how you made this!!!
 
 ### REGARDING NOTES ###
 ### Please note that uncommented notes can be found at the end of this script.
@@ -285,8 +264,8 @@ else
 			echo "* permutation analysis: failed" >> ${SUMMARY}/analysis.check.txt
 			echo "* error message: " >> ${SUMMARY}/analysis.check.txt
 			cat ${REGIONALDIR}/${STUDYNAME}_QC_qtlnom_${VARIANT}_excl_${EXCLUSION_TYPE}_PERMUTE.log | grep "ERROR" >> ${SUMMARY}/analysis.check.txt
-		    #rm -v ${REGIONALDIR}/${STUDYNAME}_QC_qtlperm_${VARIANT}_excl_${EXCLUSION_TYPE}.*
-		    #rm -v ${REGIONALDIR}/${STUDYNAME}_QC_qtlperm_${VARIANT}_excl_${EXCLUSION_TYPE}_PERMUTE.*
+# 		    rm -v ${REGIONALDIR}/${STUDYNAME}_QC_qtlperm_${VARIANT}_excl_${EXCLUSION_TYPE}.*
+# 		    rm -v ${REGIONALDIR}/${STUDYNAME}_QC_qtlperm_${VARIANT}_excl_${EXCLUSION_TYPE}_PERMUTE.*
 		    echo "${VARIANT} ${LOCUS} ${CHR} ${BP} ${START} ${END} ${WINDOWSIZE} ${TYPE} ${PHENOTYPE}" >> ${SUMMARY}/regions_for_qtl.failedPerm.temp.txt
 		fi
 
@@ -296,9 +275,9 @@ else
 
 	echo ""
 	echo "* Removing duplicates from re-run file."
-	perl ${SOFTWARE}/removedupesv2 ${SUMMARY}/regions_for_qtl.failedQTLs.temp.txt NORM ${SUMMARY}/regions_for_qtl.failedQTLs.txt
-	perl ${SOFTWARE}/removedupesv2 ${SUMMARY}/regions_for_qtl.failedNom.temp.txt NORM ${SUMMARY}/regions_for_qtl.failedNom.txt
-	perl ${SOFTWARE}/removedupesv2 ${SUMMARY}/regions_for_qtl.failedPerm.temp.txt NORM ${SUMMARY}/regions_for_qtl.failedPerm.txt
+	perl ${FASTQCTLADDON}/removedupes.pl ${SUMMARY}/regions_for_qtl.failedQTLs.temp.txt NORM ${SUMMARY}/regions_for_qtl.failedQTLs.txt
+	perl ${FASTQCTLADDON}/removedupes.pl ${SUMMARY}/regions_for_qtl.failedNom.temp.txt NORM ${SUMMARY}/regions_for_qtl.failedNom.txt
+	perl ${FASTQCTLADDON}/removedupes.pl ${SUMMARY}/regions_for_qtl.failedPerm.temp.txt NORM ${SUMMARY}/regions_for_qtl.failedPerm.txt
 	
 	echo "  - removing temporary files..."
 	rm -v ${SUMMARY}/regions_for_qtl.failedQTLs.temp.txt
